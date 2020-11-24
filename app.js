1dde770938953391e88e1cd9d174f629641a2323
nikhazy <60614819+nikhazy@users.noreply.github.com>
2020-11-24 16:18:29 +0100

    Some Try

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 var measure = require('./BackEnd/Measure');
 //var express = require('./Backend/ExpressHandler')
 const express = require('express');
+const upload = require('express-fileupload');
+
 const app = express();
 
 app.use(express.static(__dirname)); //Nem tudom ez mit csinál de király
+app.use(upload());
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/FrontEnd/index.html')
+});
+
+app.post('/', (req,res) =>{
+    if(req.files)
+    {
+        var file = req.files.file;
+        file.mv('./BackEnd/gcode.txt',function (err){
+            if(err){
+                res.send(err)
+            }
+        })
+    }
 });
 
 console.log(__dirname);
